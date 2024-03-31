b3a5ec3a36b6add25ba203d729990fe59147785c
Saadaan Hassan <webmaker9d@gmail.com>
2024-03-31 21:29:46 +0500

    Add Login/Signup using firebase

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import "react-native-gesture-handler";
-import { Text } from "react-native";
+
+import { Text, Touchable, TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather } from "@expo/vector-icons";
 
+import useAuth from "./hooks/UserHook";
+
 import HomeScreen from "./screens/HomeScreen";
 import StoreScreen from "./screens/StoreScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+import ProfileDropdown from "./components/ProfileDropdown";
 
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,10 +25,95 @@ const AboutScreen = () => {
 };
 
 export default function App() {
+	const { user } = useAuth();
+
 	return (
 		<NavigationContainer>
-			{/* Stack Navigation */}
-			{/* <Stack.Navigator
+			{user ? (
+				<Tab.Navigator
+					initialRouteName='Home'
+					screenOptions={{
+						headerStyle: {
+							backgroundColor: "#227398",
+						},
+						headerTintColor: "white",
+						headerTitleStyle: {
+							fontWeight: "bold",
+						},
+						tabBarActiveTintColor: "tomato",
+						tabBarInactiveTintColor: "white",
+						tabBarStyle: {
+							backgroundColor: "#227398",
+							padding: 10,
+							height: 60,
+						},
+						headerRight: () => {
+							return <ProfileDropdown />;
+						},
+					}}>
+					<Tab.Screen
+						name='Home'
+						component={HomeScreen}
+						options={{
+							tabBarIcon: ({ focused, color, size }) => (
+								<Feather name='home' size={size} color={color} />
+							),
+						}}
+					/>
+					<Tab.Screen
+						name='Store'
+						component={StoreScreen}
+						options={{
+							tabBarIcon: ({ focused, color, size }) => (
+								<Feather name='box' size={size} color={color} />
+							),
+						}}
+					/>
+					<Tab.Screen
+						name='About'
+						component={AboutScreen}
+						options={{
+							tabBarIcon: ({ focused, color, size }) => (
+								<Feather name='info' size={size} color={color} />
+							),
+						}}
+					/>
+				</Tab.Navigator>
+			) : (
+				<Stack.Navigator
+					initialRouteName='Login'
+					screenOptions={{
+						headerMode: "screen",
+						headerTintColor: "white",
+						headerStyle: { backgroundColor: "#227398" },
+					}}>
+					<Stack.Screen
+						name='Login'
+						component={LoginScreen}
+						options={{
+							title: "Log in",
+						}}
+					/>
+
+					<Stack.Screen
+						name='Register'
+						component={RegisterScreen}
+						options={{
+							title: "Create an Account",
+							headerLeft: null,
+						}}
+					/>
+				</Stack.Navigator>
+			)}
+		</NavigationContainer>
+	);
+}
+
+{
+	/* Stack Navigation */
+}
+{
+	/* <Stack.Navigator
 				initialRouteName='Home'
 				screenOptions={{
 					headerMode: "screen",
@@ -49,10 +141,14 @@ export default function App() {
 						gestureEnabled: false,
 					}}
 				/>
-			</Stack.Navigator> */}
+			</Stack.Navigator> */
+}
 
-			{/* Side Navigation */}
-			{/* <Drawer.Navigator
+{
+	/* Side Navigation */
+}
+{
+	/* <Drawer.Navigator
 				initialRouteName='Home'
 				screenOptions={{
 					headerStyle: {
@@ -95,55 +191,9 @@ export default function App() {
 						),
 					}}
 				/>
-			</Drawer.Navigator> */}
+			</Drawer.Navigator> */
+}
 
-			{/* Bottom Navigation */}
-			<Tab.Navigator
-				initialRouteName='Home'
-				screenOptions={{
-					headerStyle: {
-						backgroundColor: "#227398",
-					},
-					headerTintColor: "white",
-					headerTitleStyle: {
-						fontWeight: "bold",
-					},
-					tabBarActiveTintColor: "tomato",
-					tabBarInactiveTintColor: "white",
-					tabBarStyle: {
-						backgroundColor: "#227398",
-						padding: 10,
-						height: 60,
-					},
-				}}>
-				<Tab.Screen
-					name='Home'
-					component={HomeScreen}
-					options={{
-						tabBarIcon: ({ focused, color, size }) => (
-							<Feather name='home' size={size} color={color} />
-						),
-					}}
-				/>
-				<Tab.Screen
-					name='Store'
-					component={StoreScreen}
-					options={{
-						tabBarIcon: ({ focused, color, size }) => (
-							<Feather name='box' size={size} color={color} />
-						),
-					}}
-				/>
-				<Tab.Screen
-					name='About'
-					component={AboutScreen}
-					options={{
-						tabBarIcon: ({ focused, color, size }) => (
-							<Feather name='info' size={size} color={color} />
-						),
-					}}
-				/>
-			</Tab.Navigator>
-		</NavigationContainer>
-	);
+{
+	/* Bottom Navigation */
 }
