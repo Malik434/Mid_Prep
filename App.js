b933d1f9464bec7bb729af105c78fcac8f887023
Saadaan Hassan <webmaker9d@gmail.com>
2024-03-31 15:50:12 +0500

    Create Stack, Side and Bottom Navigations

--- App.js
+++ App.js
@@ -1,20 +1,125 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
+
+import HomeScreen from "./screens/HomeScreen";
+
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const DetailsScreen = () => {
+	return <Text>Details</Text>;
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	return (
+		<NavigationContainer>
+			{/* Stack Navigation */}
+			{/* <Stack.Navigator
+				initialRouteName='Home'
+				screenOptions={{
+					headerMode: "screen",
+					headerTintColor: "white",
+					headerStyle: { backgroundColor: "#227398" },
+				}}>
+				<Stack.Screen
+					name='Home'
+					component={HomeScreen}
+					options={{
+						title: "Awesome app",
+					}}
+				/>
+				<Stack.Screen
+					name='Details'
+					component={DetailsScreen}
+					options={{
+						gestureEnabled: false,
+					}}
+				/>
+			</Stack.Navigator> */}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+			{/* Side Navigation */}
+			{/* <Drawer.Navigator
+				initialRouteName='Home'
+				screenOptions={{
+					headerStyle: {
+						backgroundColor: "#227398",
+					},
+					headerTintColor: "white",
+					headerTitleStyle: {
+						fontWeight: "bold",
+					},
+					drawerStyle: {
+						backgroundColor: "#227398",
+					},
+					drawerActiveTintColor: "tomato",
+					drawerInactiveTintColor: "white",
+				}}>
+				<Drawer.Screen
+					name='Home'
+					component={HomeScreen}
+					options={{
+						drawerIcon: ({ focused, color, size }) => (
+							<Feather name='home' size={size} color={color} />
+						),
+					}}
+				/>
+				<Drawer.Screen
+					name='Details'
+					component={DetailsScreen}
+					options={{
+						drawerIcon: ({ focused, color, size }) => (
+							<Feather name='info' size={size} color={color} />
+						),
+					}}
+				/>
+			</Drawer.Navigator> */}
+
+			{/* Bottom Navigation */}
+			<Tab.Navigator
+				initialRouteName='Home'
+				tabBarOptions={{
+					activeTintColor: "tomato",
+					inactiveTintColor: "white",
+				}}
+				screenOptions={{
+					headerStyle: {
+						backgroundColor: "#227398",
+					},
+					headerTintColor: "white",
+					headerTitleStyle: {
+						fontWeight: "bold",
+					},
+					tabBarStyle: {
+						backgroundColor: "#227398",
+						padding: 10,
+						height: 60,
+					},
+				}}>
+				<Tab.Screen
+					name='Home'
+					component={HomeScreen}
+					options={{
+						tabBarIcon: ({ focused, color, size }) => (
+							<Feather name='home' size={size} color={color} />
+						),
+					}}
+				/>
+				<Tab.Screen
+					name='Details'
+					component={DetailsScreen}
+					options={{
+						tabBarIcon: ({ focused, color, size }) => (
+							<Feather name='info' size={size} color={color} />
+						),
+					}}
+				/>
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,43 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+
+export default function HomeScreen() {
+	const navigation = useNavigation();
+	return (
+		<View style={styles.container}>
+			<Text style={styles.h1}>Home</Text>
+			<Text>Navigate to the Details screen</Text>
+			<TouchableOpacity
+				style={styles.button}
+				onPress={() => navigation.navigate("Details")}>
+				<Text style={styles.buttonText}>Go to Details</Text>
+			</TouchableOpacity>
+		</View>
+	);
+}
+
+const styles = {
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#22739850",
+	},
+
+	button: {
+		backgroundColor: "#227398",
+		marginTop: 10,
+		padding: 10,
+	},
+
+	h1: {
+		fontWeight: "bold",
+		fontSize: 24,
+		marginBottom: 40,
+	},
+
+	buttonText: {
+		color: "white",
+	},
+};
